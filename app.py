b4c4dd47184233101e39b7d02d8a54d103d82d5e
Lionel Noutegne <50267901+noutegne99@users.noreply.github.com>
2021-08-27 18:47:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,9 @@ def home():
 @app.route('/predict',methods=[ 'POST'])
 def predict():
     if request.method == 'POST':
+	data1 = []
         url = request.form['url'] 
-	 
+        data1 = [('{}'.format(url))]
    ##################################################################################################################################	
     def func_urlAtSymbol(url):                                    #f2
         ats = url.count('@')
