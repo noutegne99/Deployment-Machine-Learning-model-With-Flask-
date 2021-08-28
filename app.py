2b0cd5e3caeb7742392e312c139fbd79593076c7
Lionel Noutegne <50267901+noutegne99@users.noreply.github.com>
2021-08-28 15:46:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ def predict():
 
 	########################################################	
 	
-   def selct_url2(links2, newLink):
+    def selct_url2(links2, newLink):
 		if(len(links2)>=2):
 			list_of_random_items2=random.sample(links2,2)
 			newLink2=list_of_random_items2[0]
