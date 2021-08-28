913a9b2a1c5064f0307bcc8c47be58c7e412ef57
Lionel Noutegne <50267901+noutegne99@users.noreply.github.com>
2021-08-28 12:32:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -436,9 +436,9 @@ def predict():
 		links2 =[]
 		RP2=[]
 		
-		url2 = checonnectionurll2(newLink)
+		
 		try:    
-			
+			url2 = checonnectionurll2(newLink)
 			driver2 = webdriver.Chrome('chromedriver.exe')
 			driver2.implicitly_wait(2) 
 			resp2= driver2.get(url2)
