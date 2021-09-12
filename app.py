c05535a079d887aaf8921cbeccb2cb54cfd338f3
noutegnelionel@gmail.com <you@example.com>
2021-09-11 23:59:21 -0400

    jj

--- app.py
+++ app.py
@@ -162,6 +162,16 @@ def predict():
     # chrome_options.add_argument('--headless')
 	
     chrome_options.add_argument('disable-infobars')
+
+    #before use selisiun################################################
+    from selenium import webdriver
+    import os
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     #try:
     for n in range(len(data1)):
             url = data1[n]
