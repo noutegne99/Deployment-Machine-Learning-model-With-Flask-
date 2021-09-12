c7f1a63eed5074e46c2e4ccfe08e43be9885cee2
noutegnelionel@gmail.com <you@example.com>
2021-09-12 00:44:28 -0400

    ii

--- app.py
+++ app.py
@@ -171,8 +171,8 @@ def predict():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("/WebDrive/chromedriver.exe"), chrome_options=chrome_options)
-    #driver = webdriver.Chrome('../WebDrive/chromedriver.exe')
+    #driver = webdriver.Chrome(executable_path=os.environ.get("/WebDrive/chromedriver.exe"), chrome_options=chrome_options)
+    driver = webdriver.Chrome('/WebDrive/chromedriver.exe')
     #try:
     for n in range(len(data1)):
             url = data1[n]
