71c1b5e502f3b21f09cac496e4f1dc0fed49da90
noutegnelionel@gmail.com <you@example.com>
2021-12-10 23:55:36 -0500

    app

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ def predict():
                 driver.get(url)
                 #time.sleep(3)
                 #driver.get_screenshot_as_file('/Screenshot/screenshot1.png')
-                driver.save_screenshot('screenshot1.png')
+                driver.save_screenshot('/Screenshot/screenshot1.png')
                 driver.maximize_window()
                 driver.close()
                 response = requests.get(url)
@@ -534,7 +534,7 @@ def predict():
             GFeature9 = Good_func_urLength(Feature9,Feature91,Feature92)
 
 
-            with Image.open("screenshot1.png") as H1 :
+            with Image.open("/Screenshot/screenshot1.png") as H1 :
                 row, col = dhash.dhash_row_col(H1 )
                 #hash1.show() 
                 hash1= dhash.format_hex(row, col)
